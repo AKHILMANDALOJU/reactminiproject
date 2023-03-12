c8dbc6d1afb1ed27fb8d13af4f135f895fdb5b24
AKHIL <127681261+AKHILMANDALOJU@users.noreply.github.com>
2023-03-12 22:24:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import { useState } from 'react';
+import Keypad from './keypad';
+
+function App(){
+  const [input,setInput] = useState('');
+  const handleClick = (value) => {
+    setInput(input+value);
+  };
+  const handleEqual = () => {
+    setInput(eval(input).toString());
+  };
+  const handleClear =  () => {
+    setInput("");
+  };
+  return(
+    <div className='App'>
+      <div className='Calculator'>
+        <input className='display' type="text" value={input} readOnly />
+          <Keypad
+          handleClear={handleClear}
+          handleClick={handleClick}
+          handleEqual={handleEqual}/>
+
+        
+      </div>
+    </div>
+  );
+
+}
+
+
+export default App;
